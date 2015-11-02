1f06e1e8539e29d25472ba01886b2abf101be45b
lxuad <lxuad.xlxiao28@gmail.com>
2015-11-01 23:03:46 -0500

    modify getFeature.py to read files of all classes

--- getFearure/getFeatures.py
+++ getFearure/getFeatures.py
@@ -10,47 +10,50 @@ import re
 if __name__ == '__main__':
 	vocab = {}
 	folder_path = sys.argv[1]
-	for fname in os.listdir(folder_path):
-		f = codecs.open(folder_path+'/'+fname, encoding = 'utf-8')
-		for line in f:
-			features = {}
-			sentence_type = 0
-			label = 'false'
-			token = line.rstrip().lstrip().split("\t")
-			sentence = token[0]
+	for folder in os.listdir(folder_path):
+		if "class" in folder:
+			for fname in os.listdir(folder_path+"/"+folder):
+				f = codecs.open(folder_path+'/'+fname, encoding = 'utf-8')
+				for line in f:
+					features = {}
+					sentence_type = 0
+					label = 'false'
+					token = line.rstrip().lstrip().split("\t")
+					sentence = token[0]
 
-			#get label for each sentence
-			if len(token) > 1:
-				sentence = token[1]
-				if token[0] == '-true':
-					label = "true"
-				else:
-					label = "notsure"
+					#get label for each sentence
+					if len(token) > 1:
+						sentence = token[1]
+						if token[0] == '-true':
+							label = "true"
+						else:
+							label = "notsure"
 
-			#set the sentence type: 1 for questions 2 for declarative sentence 3 for exclaimation
-			#sentence type is treated as the first feature
-			last_char = sentence[len(sentence)-1]
-			if last_char == '?':
-				sentence_type = 1
-			elif last_char == ".":
-				sentence_type = 2
-			elif last_char == "!":
-				sentence_type = 3
+					#set the sentence type: 1 for questions 2 for declarative sentence 3 for exclaimation
+					#sentence type is treated as the first feature
+					last_char = sentence[len(sentence)-1]
+					if last_char == '?':
+						sentence_type = 1
+					elif last_char == ".":
+						sentence_type = 2
+					elif last_char == "!":
+						sentence_type = 3
 
-			#only consider words that include [0-9a-zA-Z]
-			sentence = re.sub("\\W+", " ", sentence, flags = re.UNICODE)
-			words = sentence.split()
-			if len(words) < 1:
-				continue
-			for w in words:
-				if w not in vocab.keys():
-					vocab[w] = len(vocab) + 2
-				index = vocab[w]
-				if index in features.keys():
-					features[index] += 1
-				else:
-					features[index] = 1
-			sys.stdout.write(label+"\t1:"+str(sentence_type)+" ")
-			for key in sorted(features):
-				sys.stdout.write(str(key)+':'+str(features[key])+" ")
-			sys.stdout.write("\n")
+					#only consider words that include [0-9a-zA-Z]
+					sentence = sentence.replace("**", "");
+					sentence = re.sub("\\W+", " ", sentence, flags = re.UNICODE)
+					words = sentence.split()
+					if len(words) < 1:
+						continue
+					for w in words:
+						if w not in vocab.keys():
+							vocab[w] = len(vocab) + 2
+						index = vocab[w]
+						if index in features.keys():
+							features[index] += 1
+						else:
+							features[index] = 1
+					sys.stdout.write(label+"\t1:"+str(sentence_type)+" ")
+					for key in sorted(features):
+						sys.stdout.write(str(key)+':'+str(features[key])+" ")
+					sys.stdout.write("\n")
